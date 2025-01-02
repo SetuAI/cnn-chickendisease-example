25980b0369813884c69c44042299d3cbe355bc1e
Setu AI <chirantandatascience@gmail.com>
2025-01-02 09:48:08 +0500

    ingestion stage done

--- main.py
+++ main.py
@@ -3,3 +3,17 @@
 
 #from src.cnnClassifier import logger 
 #logger.info("logger test second")
+
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "DATA INGESTION STAGE"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
