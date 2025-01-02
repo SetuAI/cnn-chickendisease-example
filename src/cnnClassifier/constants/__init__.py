d05518103980ca5c53c6c9e7929a9ffa7e97379d
Setu AI <chirantandatascience@gmail.com>
2025-01-02 09:25:46 +0500

    data ingestion code uploaded

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+# set the config and patams file path here, we will be reading these yaml files
+# with read_yaml function in common.py
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+
+PARAMS_FILE_PATH = Path("params.yaml")
